cdd31323b47d3e396075a612c82a6d445caaf2ff
Sviridov-E <sviridoveg1996@gmail.com>
2026-03-28 12:41:07 +0300

    feat: auth fix

--- src/entities/auth/model.ts
+++ src/entities/auth/model.ts
@@ -18,36 +18,37 @@ export const useSessionAuthStore = create(
       refreshToken: null,
       accessToken: null,
       rememberMe: false,
-      login: ({ refreshToken, accessToken }, rememberMe) => {
-        if (rememberMe) {
-          localStorage.setItem('auth-rememberMe', 'true')
-        } else {
-          localStorage.removeItem('auth-rememberMe')
-        }
-        return set({ refreshToken, accessToken, rememberMe })
-      },
+      login: ({ refreshToken, accessToken }, rememberMe) =>
+        set({ refreshToken, accessToken, rememberMe }),
       logout: () => {
-        localStorage.removeItem('auth-rememberMe')
         set({ refreshToken: null, accessToken: null, rememberMe: false })
       },
     }),
     {
       name: 'auth-storage',
       storage: createJSONStorage(() => ({
-        getItem: name => {
-          return localStorage.getItem(name) || sessionStorage.getItem(name)
-        },
+        getItem: name =>
+          localStorage.getItem(name) || sessionStorage.getItem(name),
         setItem: (name, value) => {
-          if (localStorage.getItem('auth-rememberMe') === 'true') {
+          const { state } = JSON.parse(value)
+
+          if (!state.accessToken && !state.refreshToken) {
+            localStorage.removeItem(name)
+            sessionStorage.removeItem(name)
+            return
+          }
+
+          if (state.rememberMe) {
             localStorage.setItem(name, value)
+            sessionStorage.removeItem(name)
           } else {
             sessionStorage.setItem(name, value)
+            localStorage.removeItem(name)
           }
         },
         removeItem: name => {
           localStorage.removeItem(name)
           sessionStorage.removeItem(name)
-          localStorage.removeItem('auth-rememberMe')
         },
       })),
     }
